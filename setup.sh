14f66910cbc5cf89ec7e50823fa27e815b5debef
YingkunZhou <albertchow1997@berkeley.edu>
2019-02-15 11:56:26 +0800

    modify setup to support relative path

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ $1 == 'rv32_5stage' ]]; then
-	sbt 'runMain rv32_5stage.Top -td /home/albert/CPU/mizhi/emulator/rv32_5stage/generated-src'
+	sbt 'runMain rv32_5stage.Top -td ../mizhi/emulator/rv32_5stage/generated-src'
 elif [[ $1 == 'chiwen' ]]; then
-	sbt 'runMain chiwen.Top -td /home/albert/CPU/mizhi/emulator/chiwen/generated-src'
+	sbt 'runMain chiwen.Top -td ../mizhi/emulator/chiwen/generated-src'
 elif [[ $1 == 'fuxi' ]]; then
-	sbt 'runMain fuxi.Top -td /home/albert/CPU/mizhi/emulator/fuxi/generated-src'
+	sbt 'runMain fuxi.Top -td ../mizhi/emulator/fuxi/generated-src'
 elif [[ $1 == 'rv32_3stage' ]]; then
-	sbt 'runMain rv32_3stage.Top -td emulator/rv32_3stage/generated-src'
+	sbt 'runMain rv32_3stage.Top -td ../mizhi/emulator/rv32_3stage/generated-src'
 fi
