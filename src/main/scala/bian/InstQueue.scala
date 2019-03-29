654c8700e64c168dc197f28a9c181aa7020a8c49
YingkunZhou <albertchow1997@berkeley.edu>
2019-03-29 20:04:29 +0800

    InstQueue v0.1

--- src/main/scala/bian/InstQueue.scala
+++ src/main/scala/bian/InstQueue.scala
@@ -39,7 +39,7 @@ class ByPass(val addr_width: Int) extends Bundle {
   val addr = UInt(addr_width.W)
 }
 
-trait InstParam extends Pram {
+trait InstParam extends BackParam {
   val nEntry = 8
   val wEntry = log2Ceil(nEntry)
   val wCount = log2Ceil(nEntry+1)
@@ -47,8 +47,8 @@ trait InstParam extends Pram {
 
 class InstQueue extends Module with InstParam {
   val io = IO(new Bundle{
-    val in  = Input(Valid(new F1Issue(wPhyAddr, wOrder)))
     val ready = Output(Bool())
+    val in  = Input(Valid(new F1Issue(wPhyAddr, wOrder))) //TODO: check if kill or not
     val issue = Decoupled(new InfoIssue(wPhyAddr, wOrder, data_width))
     val data_sel = Output(Vec(2, UInt(nCommit.W)))
 
@@ -64,132 +64,161 @@ class InstQueue extends Module with InstParam {
     val kill = Input(Valid(UInt(wOrder.W)))
     val counter = Output(UInt(wCount.W))
   })
-  //begin to read Regfile
   val issue = RegInit({
     val w = Wire(Valid(new F1Issue(wPhyAddr, wOrder)))
     w.valid := false.B
     w.bits := DontCare
     w
   })
-  val queue_ctrl = Wire(new Bundle {
+  val inst_ctrl = Wire(new Bundle {
+    // for head
+    val invalid = Bool()
+    val ready = Vec(2, Bool())
+    // for queue
     val snoop = Vec(nEntry, Vec(2, Bool()))
     val limit = Vec(nEntry, Bool())
-    val empty = UInt(nEntry.W)
-    val valid = Vec(2, Bool())
-    val tail = UInt(wEntry.W)
-    val kill = new PriorityKill(nEntry)
-    val ready = UInt(nEntry.W)
+    val valid = UInt(nEntry.W)
+    val kill  = new PriorityKill(nEntry)
+    val tail  = UInt((wEntry+1).W)
+    def pop_out: Bool = (valid & forward).orR
     def forward: UInt = VecInit((0 until nEntry).map(i => snoop(i).reduce(_&&_) && !limit(i))).asUInt
-    def fwd_ptr: UInt = Mux(ready.orR, PriorityEncoder(ready), 0.U(wEntry.W))
+    def fwd_ptr: UInt = Mux(pop_out, PriorityEncoder(forward), 0.U(wEntry.W))
   })
-  io.data_sel := issue.bits.rs.map(rs => VecInit(io.bypass.map(bypass =>
-    bypass.addr === rs.addr && bypass.valid)).asUInt)
-  queue_ctrl.valid := (0 until 2).map(i => io.data_sel(i).orR || issue.bits.rs(i).valid)
-  io.pop_out.addr  := issue.bits.rd.addr
-  io.pop_out.valid := issue.bits.rd.valid && issue.bits.f1 && queue_ctrl.valid.reduce(_&&_)
-
-  io.issue.valid     := issue.valid
-  io.issue.bits.id   := issue.bits.id
-  io.issue.bits.rd   := issue.bits.rd
-  io.issue.bits.f1   := issue.bits.f1
-  io.issue.bits.mem_en := issue.bits.mem_en
-  io.issue.bits.info.op  := io.resp_in.op
-  io.issue.bits.info.pc  := io.resp_in.pc
-  io.issue.bits.info.imm := io.resp_in.imm
-  io.issue.bits.info.imm7_0 := Cat(issue.bits.rs(1).addr(7-wPhyAddr, 0), issue.bits.rs(0).addr)
-  for (i <- 0 until 2) {
-    io.issue.bits.rs(i).addr  := issue.bits.rs(i).addr
-    io.issue.bits.rs(i).valid := queue_ctrl.valid(i)
-  }
 
   val inst_count = RegInit(0.U(wCount.W))
   val inst_valid = RegInit(VecInit(Seq.fill(nEntry)(false.B)))
   val inst_queue = Reg(Vec(nEntry, new F1Issue(wPhyAddr, wOrder)))
-  queue_ctrl.empty := ~inst_valid.asUInt
-  queue_ctrl.ready := inst_valid.asUInt & queue_ctrl.forward
-  io.counter := inst_count
-  io.ready := queue_ctrl.empty.orR || io.issue.ready
 
-  val flush = io.xcpt || (CmpId(io.kill.bits, issue.bits.id, io.head_id) && io.kill.valid)
-  when (flush) {
-    issue.valid := false.B
-  }.elsewhen (issue.valid) { 
-    when (io.issue.ready) {
-      when (inst_count === 0.U) {
-        when (io.in.valid) {
-          issue := io.in
-          issue.valid := true.B
-        }.otherwise {
-          issue.valid := false.B
-        }
-      }.otherwise {
-        issue.bits  := inst_queue(queue_ctrl.fwd_ptr)
-        issue.valid := true.B
+  io.counter := inst_count
+  io.ready := ~inst_valid.asUInt.orR //|| io.issue.ready FIXME: save time and logic???
+
+  io.data_sel := issue.bits.rs.map(rs => VecInit(io.bypass.map(bypass =>
+    bypass.addr === rs.addr && bypass.valid)).asUInt)
+
+  inst_ctrl.invalid := io.kill.valid && CmpId(io.kill.bits, issue.bits.id, io.head_id)
+  inst_ctrl.ready := (0 until 2).map(i => io.data_sel(i).orR || issue.bits.rs(i).valid)
+
+  io.pop_out.addr  := issue.bits.rd.addr
+  io.pop_out.valid := issue.bits.rd.valid && issue.bits.f1 && inst_ctrl.ready.reduce(_&&_) //&& io.issue.ready->no needed
+  io.issue.valid            := issue.valid
+  io.issue.bits.id          := issue.bits.id
+  io.issue.bits.rd          := issue.bits.rd
+  io.issue.bits.f1          := issue.bits.f1
+  io.issue.bits.mem_en      := issue.bits.mem_en
+  io.issue.bits.info.op     := io.resp_in.op
+  io.issue.bits.info.pc     := io.resp_in.pc
+  io.issue.bits.info.imm    := io.resp_in.imm
+  io.issue.bits.info.imm7_0 := Cat(issue.bits.rs(1).addr(7-wPhyAddr, 0), issue.bits.rs(0).addr)
+  for (i <- 0 until 2) {
+    io.issue.bits.rs(i).addr  := issue.bits.rs(i).addr
+    io.issue.bits.rs(i).valid := inst_ctrl.ready(i)
+  }
+
+  inst_ctrl.tail := inst_count - 1.U
+  inst_ctrl.valid := inst_valid.asUInt & inst_ctrl.kill.survive
+  inst_ctrl.kill.cmp := inst_queue.map(i => CmpId(io.kill.bits, i.id, io.head_id))
+  inst_ctrl.kill.cmp_val := io.kill.valid
+  when (io.xcpt) { issue.valid := false.B
+  }.elsewhen(issue.valid) {
+    when (inst_count === 0.U) {
+      when (inst_ctrl.invalid) {
+        issue.valid := false.B
+      }.elsewhen(io.issue.ready) {
+        issue  := io.in
+      }
+    }.elsewhen(inst_ctrl.invalid || io.issue.ready) {
+      issue.bits  := inst_queue(inst_ctrl.fwd_ptr)
+      issue.valid := inst_ctrl.kill.survive(0)
+    }.otherwise {for (i <- 0 until 2)
+      issue.bits.rs(i).valid := inst_ctrl.valid(i)
+    }
+  }.elsewhen(!io.issue.ready) {
+    issue  := io.in
+  }
+
+  when (io.xcpt) { inst_count := 0.U
+  }.elsewhen (issue.valid) {
+    when (io.kill.valid && inst_ctrl.kill.cmp(0)) {
+      inst_count := 0.U
+    }.elsewhen (inst_count =/= 0.U) {
+      when (inst_ctrl.kill.valid(inst_valid.asUInt)) {
+        inst_count := inst_ctrl.kill.ptr - io.issue.ready.asUInt
+      }.elsewhen(io.issue.ready && !io.in.valid) {
+        inst_count := inst_ctrl.tail
       }
     }
-  }.elsewhen(!io.issue.ready && io.in.valid) {
-    issue := io.in
-    issue.valid := true.B
-  }.otherwise {
-    for (i <- 0 until 2) issue.bits.rs(i).valid := queue_ctrl.ready(i)
+    when(!io.issue.ready && io.in.valid) {
+      inst_count := inst_count + 1.U
+    }
   }
 
-  queue_ctrl.kill.cmp := VecInit(inst_queue.map(i => CmpId(io.kill.bits, i.id, io.head_id))).asUInt
+  def updateQueue(i: Int): Unit = {for (j <- 0 until 2) inst_queue(i).rs(j).valid := inst_ctrl.snoop(i)(j)}
+  def stable_queue: Seq[Bool] = (0 until nEntry).map(i => i.U  <  inst_ctrl.fwd_ptr)
+  def shift_queue : Seq[Bool] = (0 until nEntry).map(i => i.U  <  inst_ctrl.tail(wEntry-1,0))
+  def touch_tail  : Seq[Bool] = (0 until nEntry).map(i => i.U === inst_ctrl.tail(wEntry-1,0))
+  def touch_count : Seq[Bool] = (0 until nEntry).map(i => i.U === inst_count(wEntry-1,0))
 
-  def updateQueue(i0: Int, i1: Int): Unit = {
-    for (j <- 0 until 2) inst_queue(i0).rs(j).valid := queue_ctrl.snoop(i1)(j)
-  }
   for (i <- 0 until nEntry) {
-    when (!io.issue.fire) {
-      updateQueue(i,i)
-    }.otherwise {
-      when (i.U < queue_ctrl.fwd_ptr) {
-        updateQueue(i,i)
-      }.elsewhen (i.U < queue_ctrl.tail) {
-        if (i < nEntry-1) {
-          updateQueue(i, i + 1)
-          for (j <- 0 until 2) inst_queue(i).rs(j).addr := inst_queue(i + 1).rs(j).addr
+    when (io.xcpt) {
+      inst_valid(i) := false.B
+    }.elsewhen (issue.valid) {
+      when (io.issue.ready) {
+        when (stable_queue(i)) {
+          inst_valid(i) := inst_ctrl.valid(i)
+        }.elsewhen(shift_queue(i)) { if (i < nEntry-1)
+          inst_valid(i) := inst_ctrl.valid(i + 1)
+        }
+        when (touch_tail(i)) {
+          inst_valid(i) := io.in.valid && inst_count =/= 0.U
+        }
+      }.otherwise {
+        when(touch_count(i)) {
+          inst_valid(i) := io.in.valid
+        }.otherwise {
+          inst_valid(i) := inst_ctrl.valid(i)
+        }
+      }
+    }
+
+    when (issue.valid) {
+      when (io.issue.ready) {
+        when(stable_queue(i)){ updateQueue(i)
+        }.elsewhen(shift_queue(i)) { if (i < nEntry-1) {
+          for (j <- 0 until 2) {
+            inst_queue(i).rs(j).addr := inst_queue(i + 1).rs(j).addr
+            inst_queue(i).rs(j).valid := inst_ctrl.snoop(i + 1)(j)
+          }
           inst_queue(i).id := inst_queue(i + 1).id
           inst_queue(i).rd := inst_queue(i + 1).rd
           inst_queue(i).f1 := inst_queue(i + 1).f1
-          inst_queue(i).mem_en := inst_queue(i + 1).mem_en
+          inst_queue(i).mem_en := inst_queue(i + 1).mem_en }
         }
-      }
-    }
-    // issue_reg valid
-    when (flush || (queue_ctrl.kill.cmp(i) && io.kill.valid)) {
-      inst_valid(i) := false.B
-    }.elsewhen (io.issue.fire) {
-      if (i < nEntry-1) {
-        when(queue_ctrl.fwd_ptr <= i.U && i.U < queue_ctrl.tail) {
-          inst_valid(i) := inst_valid(i + 1)
-        }.elsewhen(i.U === queue_ctrl.tail && !io.in.valid) {
-          inst_valid(i) := false.B
+        when (touch_tail(i) && io.in.valid && inst_count =/= 0.U) {
+          inst_queue(i) := io.in.bits
+        }
+      }.otherwise {
+        when(touch_count(i) && io.in.valid) {
+          inst_queue(i) := io.in.bits
+        }.otherwise { updateQueue(i)
         }
       }
     }
     //only for load & store inst
-    queue_ctrl.limit(i) := io.issueable.valid && CmpId(io.issueable.bits, inst_queue(i).id, io.head_id) && inst_queue(i).mem_en
-    for (j <- 0 until 2) {
-      queue_ctrl.snoop(i)(j) := inst_queue(i).rs(j).valid ||
+    inst_ctrl.limit(i) := io.issueable.valid && inst_queue(i).mem_en &&
+      CmpId(io.issueable.bits, inst_queue(i).id, io.head_id)
+    for (j <- 0 until 2) inst_ctrl.snoop(i)(j) := inst_queue(i).rs(j).valid ||
         io.bypass.map( bypass => bypass.addr === inst_queue(i).rs(j).addr && bypass.valid).reduce(_||_)  ||
         io.forward.map(bypass => bypass.addr === inst_queue(i).rs(j).addr && bypass.valid).reduce(_||_)
-    }
-  }
-  
-  queue_ctrl.tail := (inst_count - 1.U)(wEntry-1,0)
-  
-  when (flush) { inst_count := 0.U
-  }.elsewhen(io.kill.valid &&
-    queue_ctrl.kill.valid(inst_valid.asUInt)) {
-    inst_count := queue_ctrl.kill.ptr
-  }.elsewhen (issue.valid) {
-    when(!io.issue.ready &&  io.in.valid) {inst_count := inst_count + 1.U }
-    when( io.issue.ready && !io.in.valid && inst_count =/= 0.U) {inst_count := queue_ctrl.tail}
   }
 
-  when (issue.valid && io.in.valid) {
-    when (!io.issue.ready) { inst_queue(inst_count(wEntry-1, 0)) := io.in.bits
-    }.elsewhen (inst_count =/= 0.U) { inst_queue(queue_ctrl.tail) := io.in.bits }
-  }
+  val ids = Wire(Vec(nEntry, UInt()))
+  ids := inst_queue.map(_.id)
+  printf(p"issue $issue\n")
+  printf(p"count $inst_count ids $ids valid $inst_valid\n")
+  printf(p"snoop ${inst_ctrl.snoop}\n")
+  printf(p"ready ${inst_ctrl.ready}\n")
+  printf(p"ready ${inst_ctrl.limit}\n")
+  val cnt = RegInit(0.U(32.W))
+  cnt := cnt + 1.U
+  printf(p"=======================cnt = $cnt=============================\n")
 }
